a498efba23c5f25bb5a8e9c40d9f675b1180c621
style77 <stylek777@gmail.com>
2023-12-10 00:33:10 +0100

    chore: setup logging in main app

--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,17 @@
+import logging
 from app.lifetime import create_app
 
 
 def main():
     app = create_app()
+
+    debug = app.config.get("DEBUG")
+    app.logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    logging.getLogger().setLevel(logging.DEBUG if debug else logging.INFO)
+
     app.run(host=app.config.get("HOST"),
             port=app.config.get("PORT"),
-            debug=app.config.get("DEBUG"))
+            debug=debug)
 
 
 if __name__ == "__main__":
