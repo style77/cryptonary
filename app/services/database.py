b77ab6223c781177f13bc48e71b649f41b5e2f5a
style77 <stylek777@gmail.com>
2023-12-09 03:06:50 +0100

    chore: database manager

---
+++ app/services/database.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+from flask_sqlalchemy import SQLAlchemy
+
+from app.models.base import Base
+
+
+def get_database():
+    return SQLAlchemy(model_class=Base)
+
+
+db = get_database()
+
+
+@contextmanager
+def transaction():
+    try:
+        yield db.session
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
