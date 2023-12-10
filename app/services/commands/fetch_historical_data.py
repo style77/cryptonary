8531c3cfa35e57c4481d67957f5d1316585ed3b5
style77 <stylek777@gmail.com>
2023-12-10 01:19:22 +0100

    chore: add threading

--- app/services/commands/fetch_historical_data.py
+++ app/services/commands/fetch_historical_data.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 import math
 from typing import List
@@ -6,9 +7,7 @@ from datetime import datetime
 from app.models.cryptocurrency import CryptoCurrency, CryptoCurrencyHistoricalPrice
 from app.services.database import db
 from flask import current_app
-from requests.exceptions import RequestException
 from app.services.commands.common import setup_logger
-
 
 API_URL = "https://api.coingecko.com/api/v3/coins/{id}/market_chart"
 
@@ -19,7 +18,10 @@ INTERVAL = 61
 
 def get_key(enough_keys: bool, i: int, keys: list):
     if enough_keys and i >= PER_MINUTE:
-        return keys[i]  # WARNING: UNTESTED - LACK OF KEYS
+        key_idx = i // PER_MINUTE
+        if key_idx >= len(keys):
+            key_idx = len(keys)
+        return keys[key_idx]  # WARNING: UNTESTED - LACK OF KEYS
     else:
         return keys[0]
 
@@ -37,7 +39,7 @@ def fetch_data(record_id, key):
     if r.status_code == 429:
         current_app.logger.warning(f"429 interuppted fetching data. Sleeping {INTERVAL} seconds.")
         time.sleep(INTERVAL)
-        return fetch_data()
+        return fetch_data(record_id, key)
 
     if not r.ok:
         r.raise_for_status()
@@ -48,62 +50,73 @@ def fetch_data(record_id, key):
 def fetch_historical_data():
     setup_logger(current_app)
 
-    # even though proxies list variable is meant to be constant
-    # it needs to be under function, because current_app can be only used in
-    # app context
     records: List[CryptoCurrency] = CryptoCurrency.query.all()
-
     keys = current_app.config["COINGECKO_API_KEYS"]
     enough_keys = True
+
     if keys and math.ceil(len(keys) / 30) < len(records):
         enough_keys = False
         current_app.logger.warning(
-            f"{len(keys)} is not enough keys to fetch data without sleeping."
+            f"{len(keys)} keys are not enough to fetch data without sleeping."
         )
 
     global_start_time = time.time()
 
-    i = 0
-    for record in records:
+    def process_record(app_context, record):
+        app_context.push()
         local_start_time = time.time()
+        i = 0
+        skipped = False
 
         key = get_key(enough_keys, i, keys)
-
         data = fetch_data(record.id, key)
 
-        # We could use transaction in here, but small piece of data is better than no data
-        for row in reversed(data["prices"]):
-            date = datetime.fromtimestamp(row[0] / 1000)
-            current_app.logger.debug(f"INSERTING {record.symbol} {date}: {row[1]}")
+        try:
+            with db.session.begin():
+                for row in reversed(data["prices"]):
+                    date = datetime.fromtimestamp(row[0] / 1000).date()
+                    current_app.logger.debug(f"Inserting {record.symbol.upper()} {date}: {row[1]}")
 
-            existing_record = CryptoCurrencyHistoricalPrice.query.filter_by(
-                currency_id=record.id, timestamp=date
-            ).first()
-            if existing_record:
-                current_app.logger.debug(f"Skipping {record.symbol}")
-                print("Skipping")
-                break
+                    existing_record = CryptoCurrencyHistoricalPrice.query.filter_by(
+                        currency_id=record.id, date=date
+                    ).first()
 
-            history = CryptoCurrencyHistoricalPrice(
-                currency_id=record.id,
-                timestamp=date,
-                price=row[1],
-            )
-            db.session.add(history)
+                    if existing_record:
+                        current_app.logger.warning(f"Skipping {record.symbol.upper()}, record for {date} already exists")
+                        skipped = True
+                        break
 
-        current_app.logger.info(
-            f"Inserted {len(data['prices'])} prices for {record.name} ({record.symbol.upper()}) since {date}"
-        )
-        current_app.logger.info(
-            f"Inserted {record.symbol} in {round(time.time() - local_start_time, 2)}s"
-        )
+                    history = CryptoCurrencyHistoricalPrice(
+                        currency_id=record.id,
+                        date=date,
+                        price=row[1],
+                    )
+                    db.session.add(history)
+                    i += 1
 
-        db.session.commit()
+                first_date = datetime.fromtimestamp(data["prices"][0][0] / 1000).date()
+                last_date = datetime.fromtimestamp(data["prices"][-1][0] / 1000).date()
+                if not skipped:
+                    current_app.logger.info(
+                        f"Inserted {i} prices for {record.name} ({record.symbol.upper()}) from {first_date} to {last_date}"
+                    )
+                    current_app.logger.info(
+                        f"Inserted {record.symbol} in {round(time.time() - local_start_time, 2)}s"
+                    )
+        finally:
+            db.session.commit()
+            db.session.close()
 
-    try:
-        current_app.logger.info(
-            f"Inserted all of cryptos in {round(time.time() - global_start_time, 2)}s"
-        )
-    except Exception as e:
-        db.session.rollback()
-        current_app.logger.error(f"Failed to commit changes to the database: {e}")
+    with ThreadPoolExecutor(max_workers=5) as executor:  # Adjust max_workers as needed
+        future_to_record = {executor.submit(process_record, current_app.app_context(), record): record for record in records}
+
+        for future in as_completed(future_to_record):
+            _ = future_to_record[future]
+            try:
+                future.result()
+            except Exception as e:
+                current_app.logger.error(f"Record processing failed: {e}")
+
+    current_app.logger.info(
+        f"Inserted all of cryptos in {round(time.time() - global_start_time, 2)}s"
+    )
