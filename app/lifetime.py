99f94ed1de49425be04c7e3e694baa34af16385a
style77 <stylek777@gmail.com>
2023-12-10 17:16:06 +0100

    chore: urls

--- app/lifetime.py
+++ app/lifetime.py
@@ -2,13 +2,15 @@ from flask import Flask
 
 from app.cli import crypto_cli
 from app.services.database import db
+from app.views.urls import views
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.settings.Settings')
 
-    # app.register_blueprint(urls, url_prefix="/")
+    for view in views:
+        app.register_blueprint(view)
 
     db.init_app(app)
 
---
+++ app/views/urls.py
@@ -0,0 +1,7 @@
+from .chart import chart_page
+from .home import home_page
+
+views = [
+    home_page,
+    chart_page
+]
