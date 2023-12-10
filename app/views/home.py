87e58bb3f285d7b95b084497b1545abec07aa29f
style77 <stylek777@gmail.com>
2023-12-10 22:41:33 +0100

    feat: home page view

--- app/views/home.py
+++ app/views/home.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, abort, render_template
 from jinja2 import TemplateNotFound
+from app.models.cryptocurrency import CryptoCurrency
 
 home_page = Blueprint("home", __name__, template_folder="templates")
 
 
 @home_page.route("/")
 def home():
+    rising_cryptocurrencies = CryptoCurrency.query.filter_by(is_rising=True).all()
+    print(rising_cryptocurrencies[0].details[0])
+
     try:
-        render_template("pages/index.html")
+        return render_template(
+            "pages/index.html", rising_cryptocurrencies=rising_cryptocurrencies
+        )
     except TemplateNotFound:
         abort(404)
